125f46495818206b0842f935a707356b97614cd4
Jordan Halterman <jordan.halterman@gmail.com>
2022-03-27 11:23:23 -0700

    Update proto build script

--- build/bin/compile_protos.sh
+++ build/bin/compile_protos.sh
@@ -7,22 +7,19 @@ go_import_paths="${go_import_paths},Mgoogle/protobuf/timestamp.proto=github.com/
 go_import_paths="${go_import_paths},Mgoogle/protobuf/duration.proto=github.com/gogo/protobuf/types"
 go_import_paths="${go_import_paths},Mgoogle/protobuf/descriptor.proto=github.com/golang/protobuf/protoc-gen-go/descriptor"
 go_import_paths="${go_import_paths},Matomix/runtime/v1/primitive.proto=github.com/atomix/atomix-runtime/api/atomix/runtime/v1"
-go_import_paths="${go_import_paths},Matomix/primitive/v1/primitive.proto=github.com/atomix/atomix-runtime/api/atomix/primitive/v1"
-go_import_paths="${go_import_paths},Matomix/primitive/v1/descriptor.proto=github.com/atomix/atomix-runtime/api/atomix/primitive/v1"
-go_import_paths="${go_import_paths},Matomix/primitive/meta/v1/object.proto=github.com/atomix/atomix-runtime/api/atomix/primitive/meta/v1"
-go_import_paths="${go_import_paths},Matomix/primitive/meta/v1/timestamp.proto=github.com/atomix/atomix-runtime/api/atomix/primitive/meta/v1"
-
-protoc -I=$proto_path \
-  --doc_out=api/atomix/controller/v1 \
-  --doc_opt=markdown,controller.md \
-  --gogofaster_out=$go_import_paths,import_path=github.com/atomix/atomix-runtime/api/atomix/controller/v1,plugins=grpc:api \
-  api/atomix/controller/v1/controller.proto
-
-protoc -I=$proto_path \
-  --doc_out=api/atomix/runtime/v1 \
-  --doc_opt=markdown,manager.md \
-  --gogofaster_out=$go_import_paths,import_path=github.com/atomix/atomix-runtime/api/atomix/runtime/v1,plugins=grpc:api \
-  api/atomix/runtime/v1/manager.proto
+go_import_paths="${go_import_paths},Matomix/runtime/primitive/v1/primitive.proto=github.com/atomix/atomix-runtime/api/atomix/runtime/primitive/v1"
+go_import_paths="${go_import_paths},Matomix/runtime/primitive/v1/descriptor.proto=github.com/atomix/atomix-runtime/api/atomix/runtime/primitive/v1"
+go_import_paths="${go_import_paths},Matomix/runtime/meta/v1/object.proto=github.com/atomix/atomix-runtime/api/atomix/runtime/meta/v1"
+go_import_paths="${go_import_paths},Matomix/runtime/meta/v1/timestamp.proto=github.com/atomix/atomix-runtime/api/atomix/runtime/meta/v1"
+go_import_paths="${go_import_paths},Matomix/runtime/counter/v1/options.proto=github.com/atomix/atomix-runtime/api/atomix/runtime/counter/v1"
+go_import_paths="${go_import_paths},Matomix/runtime/election/v1/options.proto=github.com/atomix/atomix-runtime/api/atomix/runtime/election/v1"
+go_import_paths="${go_import_paths},Matomix/runtime/indexed_map/v1/options.proto=github.com/atomix/atomix-runtime/api/atomix/runtime/indexed_map/v1"
+go_import_paths="${go_import_paths},Matomix/runtime/list/v1/options.proto=github.com/atomix/atomix-runtime/api/atomix/runtime/list/v1"
+go_import_paths="${go_import_paths},Matomix/runtime/lock/v1/options.proto=github.com/atomix/atomix-runtime/api/atomix/runtime/lock/v1"
+go_import_paths="${go_import_paths},Matomix/runtime/map/v1/options.proto=github.com/atomix/atomix-runtime/api/atomix/runtime/map/v1"
+go_import_paths="${go_import_paths},Matomix/runtime/set/v1/options.proto=github.com/atomix/atomix-runtime/api/atomix/runtime/set/v1"
+go_import_paths="${go_import_paths},Matomix/runtime/topic/v1/options.proto=github.com/atomix/atomix-runtime/api/atomix/runtime/topic/v1"
+go_import_paths="${go_import_paths},Matomix/runtime/value/v1/options.proto=github.com/atomix/atomix-runtime/api/atomix/runtime/value/v1"
 
 protoc -I=$proto_path \
   --doc_out=api/atomix/runtime/v1 \
@@ -31,79 +28,124 @@ protoc -I=$proto_path \
   api/atomix/runtime/v1/runtime.proto
 
 protoc -I=$proto_path \
-  --doc_out=api/atomix/primitive/v1 \
-  --doc_opt=markdown,primitive.md \
-  --gogofaster_out=$go_import_paths,import_path=github.com/atomix/atomix-runtime/api/atomix/primitive/v1,plugins=grpc:api \
-  api/atomix/primitive/v1/primitive.proto
+  --doc_out=api/atomix/runtime/v1 \
+  --doc_opt=markdown,runtime.md \
+  --gogofaster_out=$go_import_paths,import_path=github.com/atomix/atomix-runtime/api/atomix/runtime/v1,plugins=grpc:api \
+  api/atomix/runtime/v1/runtime.proto
 
 protoc -I=$proto_path \
-  --doc_out=api/atomix/primitive/v1 \
+  --doc_out=api/atomix/runtime/primitive/v1 \
   --doc_opt=markdown,descriptor.md \
-  --gogofaster_out=$go_import_paths,import_path=github.com/atomix/atomix-runtime/api/atomix/primitive/v1,plugins=grpc:api \
-  api/atomix/primitive/v1/descriptor.proto
+  --gogofaster_out=$go_import_paths,import_path=github.com/atomix/atomix-runtime/api/atomix/runtime/primitive/v1,plugins=grpc:api \
+  api/atomix/runtime/primitive/v1/descriptor.proto
 
 protoc -I=$proto_path \
-  --doc_out=api/atomix/primitive/meta/v1 \
+  --doc_out=api/atomix/runtime/meta/v1 \
   --doc_opt=markdown,object.md \
-  --gogofaster_out=$go_import_paths,import_path=github.com/atomix/atomix-runtime/api/atomix/primitive/meta/v1,plugins=grpc:api \
-  api/atomix/primitive/meta/v1/object.proto
+  --gogofaster_out=$go_import_paths,import_path=github.com/atomix/atomix-runtime/api/atomix/runtime/meta/v1,plugins=grpc:api \
+  api/atomix/runtime/meta/v1/object.proto
 
 protoc -I=$proto_path \
-  --doc_out=api/atomix/primitive/meta/v1 \
+  --doc_out=api/atomix/runtime/meta/v1 \
   --doc_opt=markdown,timestamp.md \
-  --gogofaster_out=$go_import_paths,import_path=github.com/atomix/atomix-runtime/api/atomix/primitive/meta/v1,plugins=grpc:api \
-  api/atomix/primitive/meta/v1/timestamp.proto
+  --gogofaster_out=$go_import_paths,import_path=github.com/atomix/atomix-runtime/api/atomix/runtime/meta/v1,plugins=grpc:api \
+  api/atomix/runtime/meta/v1/timestamp.proto
 
 protoc -I=$proto_path \
-  --doc_out=api/atomix/primitive/counter/v1 \
-  --doc_opt=markdown,service.md \
-  --gogofaster_out=$go_import_paths,import_path=github.com/atomix/atomix-runtime/api/atomix/primitive/counter/v1,plugins=grpc:api \
-  api/atomix/primitive/counter/v1/service.proto
+  --doc_out=api/atomix/runtime/counter/v1 \
+  --doc_opt=markdown,primitive.md \
+  --gogofaster_out=$go_import_paths,import_path=github.com/atomix/atomix-runtime/api/atomix/runtime/counter/v1,plugins=grpc:api \
+  api/atomix/runtime/counter/v1/primitive.proto
+protoc -I=$proto_path \
+  --doc_out=api/atomix/runtime/counter/v1 \
+  --doc_opt=markdown,options.md \
+  --gogofaster_out=$go_import_paths,import_path=github.com/atomix/atomix-runtime/api/atomix/runtime/counter/v1,plugins=grpc:api \
+  api/atomix/runtime/counter/v1/options.proto
 
 protoc -I=$proto_path \
-  --doc_out=api/atomix/primitive/election/v1 \
-  --doc_opt=markdown,service.md \
-  --gogofaster_out=$go_import_paths,import_path=github.com/atomix/atomix-runtime/api/atomix/primitive/election/v1,plugins=grpc:api \
-  api/atomix/primitive/election/v1/service.proto
+  --doc_out=api/atomix/runtime/election/v1 \
+  --doc_opt=markdown,primitive.md \
+  --gogofaster_out=$go_import_paths,import_path=github.com/atomix/atomix-runtime/api/atomix/runtime/election/v1,plugins=grpc:api \
+  api/atomix/runtime/election/v1/primitive.proto
+protoc -I=$proto_path \
+  --doc_out=api/atomix/runtime/election/v1 \
+  --doc_opt=markdown,options.md \
+  --gogofaster_out=$go_import_paths,import_path=github.com/atomix/atomix-runtime/api/atomix/runtime/election/v1,plugins=grpc:api \
+  api/atomix/runtime/election/v1/options.proto
 
 protoc -I=$proto_path \
-  --doc_out=api/atomix/primitive/indexedmap/v1 \
-  --doc_opt=markdown,service.md \
-  --gogofaster_out=$go_import_paths,import_path=github.com/atomix/atomix-runtime/api/atomix/primitive/indexedmap/v1,plugins=grpc:api \
-  api/atomix/primitive/indexedmap/v1/service.proto
+  --doc_out=api/atomix/runtime/indexed_map/v1 \
+  --doc_opt=markdown,primitive.md \
+  --gogofaster_out=$go_import_paths,import_path=github.com/atomix/atomix-runtime/api/atomix/runtime/indexed_map/v1,plugins=grpc:api \
+  api/atomix/runtime/indexed_map/v1/primitive.proto
+protoc -I=$proto_path \
+  --doc_out=api/atomix/runtime/indexed_map/v1 \
+  --doc_opt=markdown,options.md \
+  --gogofaster_out=$go_import_paths,import_path=github.com/atomix/atomix-runtime/api/atomix/runtime/indexed_map/v1,plugins=grpc:api \
+  api/atomix/runtime/indexed_map/v1/options.proto
 
 protoc -I=$proto_path \
-  --doc_out=api/atomix/primitive/list/v1 \
-  --doc_opt=markdown,service.md \
-  --gogofaster_out=$go_import_paths,import_path=github.com/atomix/atomix-runtime/api/atomix/primitive/list/v1,plugins=grpc:api \
-  api/atomix/primitive/list/v1/service.proto
+  --doc_out=api/atomix/runtime/list/v1 \
+  --doc_opt=markdown,primitive.md \
+  --gogofaster_out=$go_import_paths,import_path=github.com/atomix/atomix-runtime/api/atomix/runtime/list/v1,plugins=grpc:api \
+  api/atomix/runtime/list/v1/primitive.proto
+protoc -I=$proto_path \
+  --doc_out=api/atomix/runtime/list/v1 \
+  --doc_opt=markdown,options.md \
+  --gogofaster_out=$go_import_paths,import_path=github.com/atomix/atomix-runtime/api/atomix/runtime/list/v1,plugins=grpc:api \
+  api/atomix/runtime/list/v1/options.proto
 
 protoc -I=$proto_path \
-  --doc_out=api/atomix/primitive/lock/v1 \
-  --doc_opt=markdown,service.md \
-  --gogofaster_out=$go_import_paths,import_path=github.com/atomix/atomix-runtime/api/atomix/primitive/lock/v1,plugins=grpc:api \
-  api/atomix/primitive/lock/v1/service.proto
+  --doc_out=api/atomix/runtime/lock/v1 \
+  --doc_opt=markdown,primitive.md \
+  --gogofaster_out=$go_import_paths,import_path=github.com/atomix/atomix-runtime/api/atomix/runtime/lock/v1,plugins=grpc:api \
+  api/atomix/runtime/lock/v1/primitive.proto
+protoc -I=$proto_path \
+  --doc_out=api/atomix/runtime/lock/v1 \
+  --doc_opt=markdown,options.md \
+  --gogofaster_out=$go_import_paths,import_path=github.com/atomix/atomix-runtime/api/atomix/runtime/lock/v1,plugins=grpc:api \
+  api/atomix/runtime/lock/v1/options.proto
 
 protoc -I=$proto_path \
-  --doc_out=api/atomix/primitive/map/v1 \
-  --doc_opt=markdown,service.md \
-  --gogofaster_out=$go_import_paths,import_path=github.com/atomix/atomix-runtime/api/atomix/primitive/map/v1,plugins=grpc:api \
-  api/atomix/primitive/map/v1/service.proto
+  --doc_out=api/atomix/runtime/map/v1 \
+  --doc_opt=markdown,primitive.md \
+  --gogofaster_out=$go_import_paths,import_path=github.com/atomix/atomix-runtime/api/atomix/runtime/map/v1,plugins=grpc:api \
+  api/atomix/runtime/map/v1/primitive.proto
+protoc -I=$proto_path \
+  --doc_out=api/atomix/runtime/map/v1 \
+  --doc_opt=markdown,options.md \
+  --gogofaster_out=$go_import_paths,import_path=github.com/atomix/atomix-runtime/api/atomix/runtime/map/v1,plugins=grpc:api \
+  api/atomix/runtime/map/v1/options.proto
 
 protoc -I=$proto_path \
-  --doc_out=api/atomix/primitive/set/v1 \
-  --doc_opt=markdown,service.md \
-  --gogofaster_out=$go_import_paths,import_path=github.com/atomix/atomix-runtime/api/atomix/primitive/set/v1,plugins=grpc:api \
-  api/atomix/primitive/set/v1/service.proto
+  --doc_out=api/atomix/runtime/set/v1 \
+  --doc_opt=markdown,primitive.md \
+  --gogofaster_out=$go_import_paths,import_path=github.com/atomix/atomix-runtime/api/atomix/runtime/set/v1,plugins=grpc:api \
+  api/atomix/runtime/set/v1/primitive.proto
+protoc -I=$proto_path \
+  --doc_out=api/atomix/runtime/set/v1 \
+  --doc_opt=markdown,options.md \
+  --gogofaster_out=$go_import_paths,import_path=github.com/atomix/atomix-runtime/api/atomix/runtime/set/v1,plugins=grpc:api \
+  api/atomix/runtime/set/v1/options.proto
 
 protoc -I=$proto_path \
-  --doc_out=api/atomix/primitive/topic/v1 \
-  --doc_opt=markdown,service.md \
-  --gogofaster_out=$go_import_paths,import_path=github.com/atomix/atomix-runtime/api/atomix/primitive/topic/v1,plugins=grpc:api \
-  api/atomix/primitive/topic/v1/service.proto
+  --doc_out=api/atomix/runtime/topic/v1 \
+  --doc_opt=markdown,primitive.md \
+  --gogofaster_out=$go_import_paths,import_path=github.com/atomix/atomix-runtime/api/atomix/runtime/topic/v1,plugins=grpc:api \
+  api/atomix/runtime/topic/v1/primitive.proto
+protoc -I=$proto_path \
+  --doc_out=api/atomix/runtime/topic/v1 \
+  --doc_opt=markdown,options.md \
+  --gogofaster_out=$go_import_paths,import_path=github.com/atomix/atomix-runtime/api/atomix/runtime/topic/v1,plugins=grpc:api \
+  api/atomix/runtime/topic/v1/options.proto
 
 protoc -I=$proto_path \
-  --doc_out=api/atomix/primitive/value/v1 \
-  --doc_opt=markdown,service.md \
-  --gogofaster_out=$go_import_paths,import_path=github.com/atomix/atomix-runtime/api/atomix/primitive/value/v1,plugins=grpc:api \
-  api/atomix/primitive/value/v1/service.proto
+  --doc_out=api/atomix/runtime/value/v1 \
+  --doc_opt=markdown,primitive.md \
+  --gogofaster_out=$go_import_paths,import_path=github.com/atomix/atomix-runtime/api/atomix/runtime/value/v1,plugins=grpc:api \
+  api/atomix/runtime/value/v1/primitive.proto
+protoc -I=$proto_path \
+  --doc_out=api/atomix/runtime/value/v1 \
+  --doc_opt=markdown,options.md \
+  --gogofaster_out=$go_import_paths,import_path=github.com/atomix/atomix-runtime/api/atomix/runtime/value/v1,plugins=grpc:api \
+  api/atomix/runtime/value/v1/options.proto
